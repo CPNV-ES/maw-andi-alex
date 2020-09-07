04efd8766bbbdad77cebe21fdad7d2f50cca641c
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-09-07 10:08:20 +0200

    Created exercise/new view

---
+++ src/views/exercises_new.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <a href="/">
+        <img src="/static/logo.png" class="logo" alt="logo">
+    </a>
+    <p class="header-context"><span>Exercise: </span><span>exercise name here</span></p>
+</header>
+<div class="content">
+    <h1>New Exercise</h1>
+    <form class="form" action="/exercises" method="POST">
+        <label for="title">Title</label>
+        <input id="title" type="text" name="title">
+        <input class="button button-purple" type="submit" value="Create exercise">
+    </form>
+</div>
