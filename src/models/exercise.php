ffbd0ec1ad0ea535d79d59d60da43770b9c40a24
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-09-15 15:21:43 +0200

    Add "has many" relation to questions on Exercise model

--- src/models/exercise.php
+++ src/models/exercise.php
@@ -2,9 +2,17 @@
 
 require_once 'core/model.php';
 
+require_once 'models/question.php';
+
 class Exercise extends Model
 {
     public static $table = 'exercises';
 
     public static $primary_key = 'id';
+
+      protected static function has_many() {
+        return [
+            Question::class => 'exercises_id'
+        ];
+    }
 }
