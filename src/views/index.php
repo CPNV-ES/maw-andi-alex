38aae18cd9564adcd053df729b85423bfe77b452
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-09-15 11:18:46 +0200

    Change exercises href

--- src/views/index.php
+++ src/views/index.php
@@ -5,6 +5,6 @@
 <div class="actions">
     <a href="exercises/answer" class="button button-purple">Take an exercise</a>
     <a href="exercises/new" class="button button-orange">Create an exercise</a>
-    <a href="exercises/manage" class="button button-green">Manage an exercise</a>
+    <a href="exercises" class="button button-green">Manage an exercise</a>
 </div>
 </div>
