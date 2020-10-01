acc5aaff6ff4af5b858d93fb66d3ff34d3703407
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-01 16:13:11 +0200

    Rename exercises_field view form fields

--- src/views/exercises_fields.php
+++ src/views/exercises_fields.php
@@ -17,19 +17,19 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach($exercise->questions as $field): ?>
-                    <tr>
-                        <td><?= $field->label ?></td>
-                        <td><?= $field->type ?></td>
-                        <td>
-                            <a href="/exercises/<?= $exercise->id ?>/fields/<?= $field->id ?>/edit" class="icon-button">
-                                <img src="/static/edit.svg" alt="Manage fields">
-                            </a>
-                            <a href="/exercises/<?= $exercise->id ?>/fields/<?= $field->id ?>/delete" class="icon-button">
-                                <img src="/static/delete.svg" alt="Delete">
-                            </a>
-                        </td>
-                    </tr>
+                    <?php foreach ($exercise->questions as $field) : ?>
+                        <tr>
+                            <td><?= $field->label ?></td>
+                            <td><?= $field->type ?></td>
+                            <td>
+                                <a href="/exercises/<?= $exercise->id ?>/fields/<?= $field->id ?>/edit" class="icon-button">
+                                    <img src="/static/edit.svg" alt="Manage fields">
+                                </a>
+                                <a href="/exercises/<?= $exercise->id ?>/fields/<?= $field->id ?>/delete" class="icon-button">
+                                    <img src="/static/delete.svg" alt="Delete">
+                                </a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
@@ -37,14 +37,14 @@
         </section>
         <section>
             <h1>New Field</h1>
-            <form class="form" action="/exercises/id/fields" method="POST">
+            <form class="form" action="/exercises/<?= $exercise->id ?>/fields" method="POST">
                 <label for="label">Label</label>
                 <input id="label" type="text" name="label">
-                <label for="kind">Value kind</label>
-                <select name="kind" id="kind">
-                    <option value="single">Single line text</option>
-                    <option value="multi">List of single lines</option>
-                    <option value="list">Multi-line text</option>
+                <label for="type">Value kind</label>
+                <select name="type" id="type">
+                    <option value="single_line" selected>Single line text</option>
+                    <option value="single_line_list">List of single lines</option>
+                    <option value="multi_line">Multi-line text</option>
                 </select>
                 <input class="button button-purple" type="submit" value="Create field">
             </form>
