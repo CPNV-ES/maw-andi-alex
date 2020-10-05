08ad4d51753db595a2fdf77f9697aa1bd6e25392
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-05 10:50:12 +0200

    Add exercise results view

---
+++ src/views/exercise_results.php
@@ -0,0 +1,20 @@
+<header class="header">
+    <a href="/">
+        <img src="/static/logo.png" class="logo" alt="logo">
+    </a>
+    <p class="header-context"><span>Exercise: </span><span><?= $exercise->title ?></span></p>
+</header>
+<div class="content">
+    <table class="exercise-results">
+        <thead>
+            <th>Take</th>
+            <th>question label</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>timestamp</td>
+                <td>check</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
